224804d9339e0b649c1a98dd368662c0f742829b
chuix <carleshuix@hotmail.com>
2018-06-09 12:01:10 +0200

    Subir proyecto

---
+++ spring-mvc-demo-1/src/com/chuix/springdemo/controller/WelcomeController.java
@@ -0,0 +1,28 @@
+package com.chuix.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.chuix.service.demo.WelcomeService;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String doWelcome(Model model) {
+		
+		// 1. Retriving the process data
+		WelcomeService welcomeService = new WelcomeService();
+		List<String> welcomeMessage = welcomeService.getWelcomeMessage("Carles Huix");
+		
+		// 2. add data to the model
+		model.addAttribute("myWelcomeMessage", welcomeMessage);
+		
+		// 3. return logical view name
+		return "WelcomeNew";
+
+	}
+}
