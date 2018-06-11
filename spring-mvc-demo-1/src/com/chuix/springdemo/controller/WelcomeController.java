b182d2f05a968a7cb85d6eb622c021e2e5c4c9e3
chuix <carleshuix@hotmail.com>
2018-06-11 06:28:50 +0200

    Añadir autowiring

--- spring-mvc-demo-1/src/com/chuix/springdemo/controller/WelcomeController.java
+++ spring-mvc-demo-1/src/com/chuix/springdemo/controller/WelcomeController.java
@@ -2,20 +2,25 @@ package com.chuix.springdemo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.chuix.service.demo.GenericWelcomeService;
 import com.chuix.service.demo.WelcomeService;
 
 @Controller
 public class WelcomeController {
+	
+	@Autowired
+	private GenericWelcomeService welcomeService;
 
 	@RequestMapping("/")
 	public String doWelcome(Model model) {
 		
 		// 1. Retriving the process data
-		WelcomeService welcomeService = new WelcomeService();
+		//WelcomeService welcomeService = new WelcomeService();
 		List<String> welcomeMessage = welcomeService.getWelcomeMessage("Carles Huix");
 		
 		// 2. add data to the model
