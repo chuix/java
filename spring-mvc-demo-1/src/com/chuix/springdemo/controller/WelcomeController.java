9b05abf0aa620826bb0c235440a3801cae483a58
chuix <carleshuix@hotmail.com>
2018-06-13 06:00:56 +0200

    subir proyecto

--- spring-mvc-demo-1/src/com/chuix/springdemo/controller/WelcomeController.java
+++ spring-mvc-demo-1/src/com/chuix/springdemo/controller/WelcomeController.java
@@ -8,14 +8,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.chuix.service.demo.GenericWelcomeService;
-import com.chuix.service.demo.WelcomeService;
+//import com.chuix.service.demo.WelcomeService;
 
 @Controller
 public class WelcomeController {
-	
+
+	// No es necesario crear el objeto, añadimos una anotación para que spring lo haga por nosotros
+	// GenericWelcomeService es una interface, la usamos para definir la propiedad porque nos da mas 
+	// flexibilidad. welcomeService será el modelo
 	@Autowired
 	private GenericWelcomeService welcomeService;
 
+	// Con esta anotación establecemos el patron.
+	// la url http://localhost:8080/spring-mvc-demo-1/ cumple este patron
 	@RequestMapping("/")
 	public String doWelcome(Model model) {
 		
@@ -27,7 +32,9 @@ public class WelcomeController {
 		model.addAttribute("myWelcomeMessage", welcomeMessage);
 		
 		// 3. return logical view name
-		return "WelcomeNew";
+		// Será el nombre de la vista que tenemos que devolver. Spring le añade el prefijo y sufijo
+		// para completar una url completa.
+		return "welcomeNew";
 
 	}
 }
